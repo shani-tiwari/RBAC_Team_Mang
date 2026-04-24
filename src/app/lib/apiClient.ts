e7eb948e4013f6fabc665851c4e67bca075786e6
shani-tiwari <shanitiwari2021@gmail.com>
2026-04-24 15:46:04 +0530

    api setup

---
+++ src/app/lib/apiClient.ts
@@ -0,0 +1,110 @@
+const base_api = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+
+class ApiClient{
+    private baseUrl: string;
+    
+    constructor() {
+        this.baseUrl = base_api;
+    };
+
+    /**
+     * Generic request method for api calling
+     * @template T
+     * @param {string} endpoint
+     * @param {RequestInit} options
+     * @returns {Promise<T>}
+     */
+    async request<T> (endpoint: string, options: RequestInit = {}) {
+        try {
+            const url = `${this.baseUrl}/${endpoint}`;
+
+        const defaultOptions: RequestInit = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",  // ! important for including cookies
+            ...options,
+        };
+
+        const res = await fetch(url, defaultOptions);
+        // handle response status code - 401, 403
+        if(res.status === 401 || res.status === 403) {
+            // redirect to login
+            window.location.href = "/login";
+        }
+
+        if(!res.ok) {
+            throw new Error(`API error: ${res.statusText}`);
+        }
+        
+        // const data = await res.json();
+
+        // if (!res.ok) {
+        //     throw new Error(data?.message || `API error: ${res.statusText}`);
+        // }
+
+        // return data as T;
+    } catch (error) {
+        throw error;
+    }   
+    }
+
+//  auth methods
+    // GET REQUEST
+    async register(data: Record<string, unknown>) {
+        return this.request("auth/register", {
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+    }
+
+    // login request
+    async login(email: string, password: string) {
+        return this.request("auth/login", {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+        });
+    }
+    
+    // logout request
+    async logout() {
+        return this.request("auth/logout", {
+            method: "POST",
+        }); 
+    };
+
+    //  me 
+    async getCurrentUser() {
+        return this.request("auth/me", {
+            method: "GET",
+        });
+    }; 
+
+//  user methods
+    // get users
+    async getUsers() {
+        return this.request("users", {
+            method: "GET",
+        });
+    } 
+//  admin methods
+    // update user role
+    async updateUserRole(id: string, role: string) {
+        return this.request(`users/${id}/role`, {
+            method: "PATCH",
+            body: JSON.stringify({role}),
+        });
+    }
+
+    // update user role
+    async assignUserToTeam(id: string, teamId: string) {
+        return this.request(`users/${id}/team`, {
+            method: "PATCH",
+            body: JSON.stringify({teamId}),
+        });
+    }
+
+    
+    
+
+}
