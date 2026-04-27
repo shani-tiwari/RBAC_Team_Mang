0b48d582dc4ca19cd44e3d60f6df5f42123996eb
shani-tiwari <shanitiwari2021@gmail.com>
2026-04-27 10:28:33 +0530

    type error fix

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,4 +1,35 @@
-import { Role, User } from "@prisma/client";
+import { User } from "@prisma/client";
+
+
+
+export enum Role {
+    ADMIN = "ADMIN",
+    MANAGER = "MANAGER",
+    USER = "USER",
+    GUEST = "GUEST"
+};
+
+export interface user{
+    id: string,
+    name: string,
+    email: string,
+    role: Role,
+    team?: Team,
+    teamId?: string,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+export interface Team{
+    id: string,
+    name: string,
+    members: user[],
+    code: string,
+    description?: string | null,
+    createdAt: Date,
+    updatedAt: Date
+} 
+
 
 export type LoginState = {
     success?: boolean;
@@ -19,3 +50,4 @@ export interface AuthContextType {
     isPending: boolean;
     loginState: LoginState;
 }
+
